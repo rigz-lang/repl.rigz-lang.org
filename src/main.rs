398ff1e595381d09a914be542b7168b13bb0a1e9
magicfoodhand <mitch@inapinch.io>
2025-01-10 14:24:03 -0700

    use 0.5.0

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use leptos::*;
 use leptos_meta::*;
 use rigz_runtime::eval;
+static DEFAULT_INPUT: &str = r#"
+mut a = 1
+bar = do
+    a += 1
+    21 * a
+end
+
+fn foo = bar
+
+@test
+fn test_foo
+  mut a = 1 # variables in main scope are not available for tests, this will be fixed in a later version
+  bar = do
+    a += 1
+    21 * a
+  end
+
+  assert_eq foo, 42
+  # scopes are only processed once
+  assert_eq foo, 42
+end
+
+foo
+"#;
+
 use rigz_runtime::runtime::test;
 
 #[component]
 fn App() -> impl IntoView {
-    let (contents, set_contents) = create_signal("puts 2 + 2".to_string());
+    let (contents, set_contents) = create_signal(DEFAULT_INPUT.trim().to_string());
     let (results, set_result) = create_signal(String::new());
     let (is_dark, set_is_dark) = create_signal(true); // Manual toggle for dark mode
 
