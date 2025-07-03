0195b115b921504f15d3b59988bb813ec4f8ac5b
magicfoodhand <mitch@inapinch.io>
2025-07-02 20:45:15 -0600

    handle dark mode in html and leptops, syntax highlighting, examples, smaller components, always use latest version of rigz for repl

---
+++ src/code.rs
@@ -0,0 +1,94 @@
+use gloo_utils::format::JsValueSerdeExt;
+use js_sys::{
+    Object,
+    Reflect::{get, set},
+    Function
+};
+use leptos::{component, view, IntoView};
+use leptos::html::InnerHtml;
+use leptos::prelude::{ReadSignal, WriteSignal};
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+use wasm_bindgen::closure::WasmClosureFnOnce;
+// modified from https://github.com/leptos-rs/leptos/blob/main/examples/axum_js_ssr/src/hljs.rs
+
+// node_module has default export, need to rename to defaultMod
+// todo there should be a way to use default namespace
+#[wasm_bindgen(
+    module = "/public/js/highlight.min.js"
+)]
+extern "C" {
+    type HighlightOptions;
+
+    #[wasm_bindgen(catch, js_namespace = defaultMod, js_name = highlight)]
+    fn highlight_lang(
+        code: String,
+        highlighter: Object,
+    ) -> Result<Object, JsValue>;
+
+    #[wasm_bindgen(catch, js_namespace = defaultMod, js_name = registerLanguage)]
+    fn register(
+        language: String,
+        options: JsValue,
+    ) -> Result<(), JsValue>;
+}
+
+#[wasm_bindgen(
+    module = "/public/js/rigz.js"
+)]
+extern "C" {
+    #[wasm_bindgen(catch, js_name = rigz)]
+    fn rigz_lang(
+        highlighter: Object,
+    ) -> Result<JsValue, JsValue>;
+}
+
+pub fn register_rigz() {
+    register("rigz".to_string(), rigz_lang.into_js_function())
+        .expect("failed to register language")
+}
+
+pub fn highlight(code: String, lang: String) -> Option<String> {
+    let options = Object::new();
+    set(&options, &"language".into(), &lang.into())
+        .expect("failed to assign lang to options");
+    highlight_lang(code, options)
+        .map(|result| {
+            let value = get(&result, &"value".into())
+                .expect("HighlightResult failed to contain the value key");
+            value.into_serde().expect("Value should have been a string")
+        })
+        .ok()
+}
+
+use leptos::prelude::*;
+
+// modified from https://github.com/uiwjs/react-textarea-code-editor/blob/main/core/src/Editor.tsx
+#[component]
+pub fn CodeEditor(
+    contents: ReadSignal<String>,
+    #[prop(optional)]
+    set_contents: Option<WriteSignal<String>>
+) -> impl IntoView {
+    view! {
+        <div class="container min-h-[450px] w-full border border-gray-300 dark:border-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 ">
+            <Show when={move || set_contents.is_some()}>
+                <textarea
+                    class="resize-none p-4"
+                    placeholder="Write your Rigz code here..."
+                    spellcheck="false"
+                    on:input=move |ev| set_contents.unwrap().set(event_target_value(&ev))
+                >
+                    { move || contents.get() }
+                </textarea>
+            </Show>
+
+            <div class="p-4">
+                <pre aria_hidden={"true"} class="language-rigz font-mono">
+                    <code inner_html={move || highlight(contents.get(), "rigz".to_string()).into_render()} />
+                    <br />
+                </pre>
+            </div>
+        </div>
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use leptos::*;
+mod code;
+
 use leptos_meta::*;
+use leptos::{mount::mount_to_body, prelude::*, svg};
 use rigz_runtime::eval;
-static DEFAULT_INPUT: &str = r#"
-mut a = 1
+use icondata::{LuSun, LuMoon};
+
+static ERRORS_INPUT: &str = r#"fn foo = raise "foo failed"
+
+bar = foo catch = "hello"
+
+baz = foo catch
+    1 + 2
+end
+
+bar + baz
+"#;
+
+static TESTS_INPUT: &str = r#"mut a = 1
 bar = do
     a += 1
     21 * a
@@ -26,94 +40,230 @@ end
 foo
 "#;
 
+static PROCESSES_INPUT: &str = r#"a = spawn do
+    "first"
+end
+
+b = spawn do
+    "second"
+end
+
+receive [a, b]
+"#;
+
 use rigz_runtime::runtime::test;
+use crate::code::{register_rigz, CodeEditor};
 
+/// The Icon component, modified from https://github.com/carloskiki/leptos-icons/blob/main/src/lib.rs
 #[component]
-fn App() -> impl IntoView {
-    let (contents, set_contents) = create_signal(DEFAULT_INPUT.trim().to_string());
-    let (results, set_result) = create_signal(String::new());
-    let (is_dark, set_is_dark) = create_signal(true); // Manual toggle for dark mode
-
-    view! {
-        <Html lang="en" dir="ltr" attr:data-theme=move || if is_dark() { "dark" } else { "light" } class=move || if is_dark() { "dark" } else { "" } />
-
-        <Title text="rigz REPL"/>
-
-        <Meta charset="UTF-8"/>
-        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-        <section class="flex flex-col items-center gap-4 p-6 min-h-screen bg-gray-50 dark:bg-gray-900">
-            <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">Rigz REPL</h1>
-
-            <textarea
-                class="w-full max-w-2xl h-48 p-4 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 resize-none bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 font-mono"
-                placeholder="Write your code here..."
-                on:change=move |ev| set_contents(event_target_value(&ev))
-            >
-                { move || contents() }
-            </textarea>
-
-            <p class="text-gray-600 dark:text-gray-400">All print/log output is shown in JavaScript console</p>
-
-            <div class="container mx-auto">
-                <p class="text-lg font-semibold text-gray-800 dark:text-gray-100">Output</p>
-                <p class="w-full p-4 mt-2 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md text-gray-800 dark:text-gray-100 font-mono whitespace-pre-wrap">
-                    { move || results() }
-                </p>
-            </div>
-
-            <div class="flex space-x-4 mt-4">
-                <button
-                    class="px-4 py-2 bg-green-500 text-white font-semibold rounded-md shadow hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
-                    on:click=move |_| {
-                        set_result(eval(contents().as_str())
-                            .map_err(|e| format!("Error: {e:?}"))
-                            .map(|v| v.to_string())
-                            .unwrap_or_else(|err| err)
-                        )
-                    }
-                >
-                    Run
-                </button>
-
-                <button
-                    class="px-4 py-2 bg-yellow-500 text-white font-semibold rounded-md shadow hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                    on:click=move |_| {
-                        set_result(test(contents().as_str())
-                            .map_err(|e| format!("Error: {e:?}"))
-                            .map(|v| v.to_string())
-                            .unwrap_or_else(|err| err)
-                        )
-                    }
-                >
-                    Test
-                </button>
-            </div>
-            <footer class="w-full max-w-2xl mt-8 border-t border-gray-300 dark:border-gray-700 pt-4 flex justify-between text-gray-600 dark:text-gray-400">
-                <button
-                    class="mb-4 px-3 py-1 text-sm font-semibold bg-blue-500 text-white rounded-md shadow hover:bg-blue-600 focus:outline-none"
-                    on:click=move |_| set_is_dark(!is_dark())
-                    aria-label=move || if is_dark() { "Switch to Light Mode" } else { "Switch to Dark Mode" }
-                >
-                    { move || if is_dark() {
-                        "Light"
-                    } else {
-                        "Dark"
-                    }}
-                </button>
-                <a href="//rigz-lang.org" class="hover:text-blue-500" rel="external">Rigz</a>
-                <a href="//docs.rigz-lang.org" class="hover:text-blue-500" rel="external">Docs</a>
-            </footer>
-        </section>
+pub fn Icon(
+    /// The icon to render.
+    #[prop(into)]
+    icon: Signal<icondata::Icon>,
+    #[prop(into, optional)] style: MaybeProp<String>,
+    #[prop(into, optional)] width: MaybeProp<String>,
+    #[prop(into, optional)] height: MaybeProp<String>,
+    #[prop(into, optional)] class: MaybeProp<String>,
+) -> impl IntoView {
+    move || {
+        let icon = icon.get();
+        svg::svg()
+            .style(match (style.get(), icon.style) {
+                (Some(a), Some(b)) => Some(format!("{b} {a}")),
+                (Some(a), None) => Some(a),
+                (None, Some(b)) => Some(b.to_string()),
+                _ => None,
+            })
+            .attr("x", icon.x)
+            .attr("y", icon.y)
+            .attr("width", width.get().unwrap_or_else(|| "1em".to_string()))
+            .attr("height", height.get().unwrap_or_else(|| "1em".to_string()))
+            .attr("viewBox", icon.view_box)
+            .attr("stroke-linecap", icon.stroke_linecap)
+            .attr("stroke-linejoin", icon.stroke_linejoin)
+            .attr("stroke-width", icon.stroke_width)
+            .attr("stroke", icon.stroke)
+            .attr("fill", icon.fill.unwrap_or("currentColor"))
+            .attr("role", "graphics-symbol")
+            .class(class.get().unwrap_or_default())
+            .inner_html(icon.data)
     }
 }
 
+#[component]
+fn Results(is_test: ReadSignal<bool>, results: ReadSignal<String>) -> impl IntoView {
+    view! {
+        <textarea
+            class="w-full h-32 p-4 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md text-gray-800 dark:text-gray-100 font-mono text-sm whitespace-pre-wrap resize-none"
+            readonly
+        >
+            { move || results.get() }
+        </textarea>
+    }
+}
+
+#[component]
+fn Header(is_dark: ReadSignal<bool>, set_is_dark: WriteSignal<bool>) -> impl IntoView {
+    view! {
+        <header class="bg-white dark:bg-gray-800 shadow-sm">
+            <div class="max-w-7xl mx-auto md:px-6 py-4">
+                <div class="flex flex-col md:flex-row justify-between items-center gap-4">
+                    <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">Rigz REPL</h1>
+                    <nav class="mx-auto md:ml-auto flex flex-wrap justify-center gap-6 text-gray-800 dark:text-gray-100 md:mr-6">
+                        <a href="https://rigz-lang.org" class="hover:text-blue-500 transition-colors" rel="external">Rigz</a>
+                        <a href="https://docs.rigz-lang.org" class="hover:text-blue-500 transition-colors" rel="external">Docs</a>
+                        <a href="https://gitlab.com/rigz_lang/repl.rigz-lang.org" class="hover:text-blue-500 transition-colors" rel="external">Gitlab</a>
+                    </nav>
+                    <button
+                        class="px-3 py-1 text-sm font-semibold outline-none focus:outline-none dark:text-white hover:opacity-50"
+                        on:click=move |_| set_is_dark.set(!is_dark.get())
+                        aria-label=move || if is_dark.get() { "Switch to Light Mode" } else { "Switch to Dark Mode" }
+                    >
+                        { move || if is_dark.get() { view! { <Icon icon=LuSun height="1.5rem" width="1.5rem"/> } } else { view! { <Icon icon=LuMoon height="1.5rem" width="1.5rem"/> }  }}
+                    </button>
+                </div>
+            </div>
+        </header>
+    }
+}
+
+#[component]
+fn Footer() -> impl IntoView {
+    view! {
+        <footer class="bg-white dark:bg-gray-800 shadow-sm">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+                <div class="flex flex-wrap justify-center gap-6 text-sm text-gray-600 dark:text-gray-400">
+                    <a href="https://rigz-lang.org" class="hover:text-blue-500 transition-colors" rel="external">Rigz Language</a>
+                    <a href="https://docs.rigz-lang.org" class="hover:text-blue-500 transition-colors" rel="external">Documentation</a>
+                    <a href="https://gitlab.com/rigz_lang/repl.rigz-lang.org" class="hover:text-blue-500 transition-colors" rel="external">Source Code</a>
+                </div>
+            </div>
+        </footer>
+    }
+}
+
+fn set_example_input(value: String, set_contents: WriteSignal<String>) {
+    let input = match value.as_str() {
+        "test" => TESTS_INPUT,
+        "errors" => ERRORS_INPUT,
+        "processes" => PROCESSES_INPUT,
+        _ => return
+    };
+    set_contents.set(input.to_string())
+}
+
+#[component]
+fn Main() -> impl IntoView {
+    let (contents, set_contents) = signal(TESTS_INPUT.trim().to_string());
+    let (results, set_result) = signal(String::new());
+    let (is_test, set_is_test) = signal(false);
+
+    view! {
+        <main class="flex-1 max-w-7xl w-full mx-auto md:px-4 py-6">
+                <div class="flex flex-wrap gap-3 justify-end mb-6 max-sm:px-4">
+                    <button
+                        class="flex-1 sm:flex-none px-6 py-1 bg-green-500 text-white font-semibold rounded-md shadow hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-green-400 transition-colors"
+                        on:click=move |_| {
+                            set_is_test.set(false);
+                            set_result.set(eval(contents.get())
+                                .map_err(|e| format!("Error: {e}"))
+                                .map(|v| v.to_string())
+                                .unwrap_or_else(|err| err)
+                            )
+                        }
+                    >
+                        Run
+                    </button>
+                    <button
+                        class="flex-1 sm:flex-none px-6 py-1 bg-yellow-500 text-gray-900 font-semibold rounded-md shadow hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition-colors"
+                        on:click=move |_| {
+                            set_is_test.set(true);
+                            set_result.set(test(contents.get())
+                                .map_err(|e| format!("Error: {e}"))
+                                .map(|v| v.to_string())
+                                .unwrap_or_else(|err| err)
+                            )
+                        }
+                    >
+                        Test
+                    </button>
+                    <button
+                        class="flex-1 sm:flex-none px-6 py-1 bg-gray-800 text-white font-semibold rounded-md shadow hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-gray-900 transition-colors"
+                        on:click=move |_| {
+                            set_contents.set(rigz_ast::format(contents.get()))
+                        }
+                    >
+                        Format
+                    </button>
+                </div>
+                <div class="flex flex-col h-full space-y-6">
+                    <div class="flex-1 bg-white dark:bg-gray-800 md:rounded-lg shadow-sm p-4 flex flex-col min-h-[550px]">
+                        <div class="md:flex items-center justify-between mb-2 gap-2">
+                            <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-100">Editor</h2>
+                            <div class="flex items-center space-x-2">
+                                <p class="text-sm text-gray-500 dark:text-gray-400">All print/log output is shown in JavaScript console</p>
+                            </div>
+                        </div>
+                        <div class="w-max flex gap-4 mb-2 items-center">
+                            <h3 class="text-lg font-semibold">Examples</h3>
+                            <select on:change=move |x| set_example_input(event_target_value(&x), set_contents)>
+                                <option value="test">Test</option>
+                                <option value="errors">Errors</option>
+                                <option value="processes">Processes</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-1 w-full my-2">
+                            <CodeEditor contents={contents} set_contents={set_contents} />
+                        </div>
+                    </div>
+
+                    <div class="bg-white dark:bg-gray-800 md:rounded-lg shadow-sm p-4">
+                        <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-100 mb-2">Output</h2>
+                        <Results is_test={is_test} results={results}/>
+                    </div>
+                </div>
+            </main>
+    }
+}
+
+#[component]
+fn App() -> impl IntoView {
+    provide_meta_context();
+    let (is_dark, set_is_dark) = signal(true);
+
+    view! {
+        <Html
+            {..}
+            lang="en"
+            dir="ltr"
+            class=("dark", move || is_dark.get())
+        />
+        <Title text="Rigz REPL - Online Code Editor"/>
+        <Meta charset="UTF-8"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Meta name="description" content="Online REPL for the Rigz programming language"/>
+
+        <div class="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
+            <Header is_dark={is_dark} set_is_dark={set_is_dark} />
+            <Main />
+            <Footer />
+        </div>
+    }
+}
+
+#[cfg(target_family = "wasm")]
+unsafe extern "C" {
+    fn __wasm_call_ctors();
+}
 
 fn main() {
-    // min level must be info to ensure print output is displayed
-    let _ = console_log::init_with_level(log::Level::Debug);
+    #[cfg(target_family = "wasm")]
+    unsafe {
+        __wasm_call_ctors();
+    }
+
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! {
-        <App/>
-    })
+    let _ = console_log::init_with_level(log::Level::Info);
+    register_rigz();
+    mount_to_body(App)
 }
